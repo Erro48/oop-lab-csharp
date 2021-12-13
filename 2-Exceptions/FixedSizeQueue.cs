b0a12960e237014079250a64f1ec13e192b5d812
Enrico Tagliaferri - enrico.tagliaferri2@studio.unibo.it <enrico.tagliaferri2@studio.unibo.it>
2021-12-13 11:28:48 +0100

    Make queue throws exceptions if it is full or empty

--- 2-Exceptions/FixedSizeQueue.cs
+++ 2-Exceptions/FixedSizeQueue.cs
@@ -6,13 +6,13 @@ namespace Exceptions
         private object[] _items;
         private int _firstIndex = 0;
         private int _lastIndex = 0;
-        
+
         public FixedSizeQueue(uint capacity)
         {
             Capacity = capacity;
             _items = new object[capacity];
         }
-        
+
         public uint Capacity { get; }
 
         public uint Count => (uint) (_lastIndex - _firstIndex);
@@ -20,14 +20,21 @@ namespace Exceptions
         public object GetFirst()
         {
             // TODO ensure objects can only be retrieve if the item is queue is not empty
+            if (_firstIndex == 0)
+            {
+                throw new EmptyQueueException();
+            }
             var first = _items[_firstIndex % Capacity];
             _firstIndex++;
             return first;
         }
-        
+
         public void AddLast(object item)
         {
-            // TODO ensure objects can only be inserted if the item is queue is not full
+            if (_lastIndex == Capacity)
+            {
+                throw new FullQueueException();
+            }
             _items[_lastIndex % Capacity] = item;
             _lastIndex++;
         }
